f1661be6ca4c9e977ec4490ccf6b454728efcbd0
Dean Attali <daattali@gmail.com>
2016-07-25 18:46:16 -0700

    new shiny app look

---
+++ NEWS.md
@@ -0,0 +1,6 @@
+# lightsout 0.3
+
+2016-07-25
+
+- New shiny app look
+- Submit to CRAN
--- inst/shiny/ui.R
+++ inst/shiny/ui.R
@@ -6,9 +6,19 @@ fluidPage(
   tags$head(
     tags$link(href = "style.css", rel = "stylesheet")
   ),
-  div(
-    id = "titlePanel",
-    "Lights Out"
+  div(id = "header",
+      div(id = "title",
+          "Lights Out"
+      ),
+      div(id = "subsubtitle",
+          "Created by",
+          tags$a(href = "http://deanattali.com/", "Dean Attali"),
+          HTML("&bull;"),
+          "Package available",
+          tags$a(href = "https://github.com/daattali/lightsout", "on GitHub"),
+          HTML("&bull;"),
+          tags$a(href = "http://daattali.com/shiny/", "More apps"), "by Dean"
+      )
   ),
   fluidRow(
     column(4, wellPanel(
@@ -24,13 +34,7 @@ fluidPage(
       p("Lights Out is a puzzle game consisting of a grid of lights that",
       "are either", em("on"), "(light green) or", em("off"), "(dark green). In", em("classic"), "mode, pressing any light will toggle",
       "it and its adjacent lights. In", em("variant"), "mode, pressing a light will toggle all the lights in its",
-      "row and column. The goal of the game is to", strong("switch all the lights off.")),
-      span(
-        id = "created-by-dean",
-        "Created by", a("Dean Attali", href = "http://deanattali.com"),
-        HTML("&bull;"), "Code",
-        a("on GitHub", href = "https://github.com/daattali/lightsout")
-      )
+      "row and column. The goal of the game is to", strong("switch all the lights off."))
     )),
     column(8,
       hidden(div(id = "congrats", "Good job, you won!")),
